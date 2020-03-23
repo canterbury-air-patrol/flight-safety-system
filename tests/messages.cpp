eaf0325282ac00973cb708a467bc773964fc0842
Scott Parlane <scott@parlanenz.com>
2020-03-23 09:57:23 +0000

    tests: switch to detected catch file

--- tests/messages.cpp
+++ tests/messages.cpp
@@ -1,5 +1,13 @@
+#include "config.h"
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#ifdef HAVE_CATCH2_CATCH_HPP
 #include <catch2/catch.hpp>
+#elif HAVE_CATCH_CATCH_HPP
+#include <catch/catch.hpp>
+#else
+#error No catch header
+#endif
 
 #include <cmath>
 
