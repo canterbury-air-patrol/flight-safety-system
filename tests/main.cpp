955c3ea75e7021dd7df184239e371569a0ed061a
Scott Parlane <sjp@canterburyairpatrol.org>
2024-10-27 16:50:56 +1300

    Support ubuntu noble (#14)
    * Optionally link against libgnutlsxx
    
    In ubuntu noble this library no longer exists, but the header file
    works without the library
    
    * Switch to the c++14 standard
    
    catch2 v3 now requires c++14
    
    * Support catch2 v3
    
    Based on the transition guide changing the header file and
    linking against the library from pkg-config should be similar
    to catch v2
    
    * Don't require libgnutlsxx on ubuntu noble
    
    This library doesn't exist in noble
    
    Also, fix the install path of systemd service

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#ifdef HAVE_CATCH2_CATCH_HPP
+#ifdef HAVE_CATCH2_CATCH_ALL_HPP
+#include <catch2/catch_all.hpp>
+#elif HAVE_CATCH2_CATCH_HPP
 #include <catch2/catch.hpp>
 #elif HAVE_CATCH_CATCH_HPP
 #include <catch/catch.hpp>
--- tests/messages.cpp
+++ tests/messages.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib>
 #include <memory>
 
-#ifdef HAVE_CATCH2_CATCH_HPP
+#ifdef HAVE_CATCH2_CATCH_ALL_HPP
+#include <catch2/catch_all.hpp>
+#elif HAVE_CATCH2_CATCH_HPP
 #include <catch2/catch.hpp>
 #elif HAVE_CATCH_CATCH_HPP
 #include <catch/catch.hpp>
