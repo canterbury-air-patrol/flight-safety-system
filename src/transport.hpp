99e5a177e8f2f616b7532ca0063b19a976a9e2e1
Scott Parlane <scott@parlanenz.com>
2026-03-13 21:41:42 +1300

    Remove unused ntohll define

--- src/transport.hpp
+++ src/transport.hpp
@@ -1,11 +1,4 @@
 #include <string>
-#if __APPLE__
-/* Apple already has these defines */
-#else
-#include <endian.h>
 #include <cstdint>
-static inline auto ntohll(uint64_t x) -> uint64_t { return be64toh(x); }
-static inline auto htonll(uint64_t x) -> uint64_t { return htobe64(x); }
-#endif
 
 auto convert_str_to_sa(const std::string &addr, uint16_t port, struct sockaddr_storage *sa) -> bool;
